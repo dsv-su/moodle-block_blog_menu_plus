09cd8c682748d6f7eb68166989fda49bf71159aa
simba909 <simonjarbrant@gmail.com>
2013-02-03 19:39:12 +0100

    Added addinstance for Moodle 2.4

---
+++ db/access.php
@@ -0,0 +1,13 @@
+<?php
+$capabilities = array(
+ 
+    'block/blog_menu_plus:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+ 
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+);
